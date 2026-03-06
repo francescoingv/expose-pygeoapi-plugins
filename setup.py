5c387eb7ec237807f5607fcc010c1ebbd5125efe
FrancescoIngv <FrancescoIngv@users.noreply.github.com>
2026-03-06 15:12:08 +0100

    Moved setup.py settings into project.toml

--- setup.py
+++ setup.py
@@ -1,31 +1,6 @@
-from setuptools import setup, find_packages
-from pathlib import Path
+from setuptools import setup
 
-# Legge il README per la descrizione lunga
-this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text(encoding="utf-8")
+setup()
 
-setup(
-    name = "ingv-pygeoapi-process-plugins",                # Nome del pacchetto
-    version="1.0.0",                # Versione iniziale
-    description="Plugin INGV per pygeoapi",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    author="Francesco Martinelli",
-    author_email="francesco.martinelli@ingv.it",
-    url="https://github.com/francescoingv/ingv-pygeoapi-process-plugins",
-    packages=find_packages(include=["ingv_plugin_pygeoapi", "ingv_plugin_pygeoapi.*"]),  # include pygeoapi e sottopacchetti
-    python_requires=">=3.12",         # versione minima di Python
-    install_requires=[               # dipendenze richieste
-        # "requests>=2.28",
-        # "numpy>=1.25",
-    ],
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    include_package_data=True,       # include anche file non .py come dati
-    zip_safe=False,
-)
+
 
